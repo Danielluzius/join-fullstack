7cdcd6d81c2ca06797f69d87d4c9b67761a5dafc
Kajanan Yoganathan <kaja_nan@hotmail.de>
2025-11-04 13:20:15 +0100

    fix: summary proportions aligned to mockup

--- src/app/features/summary/summary.ts
+++ src/app/features/summary/summary.ts
@@ -7,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrl: './summary.scss'
 })
 export class Summary {
+  todoIconSrc: string = 'assets/summary/edit-white-signup.png';
+  doneIconSrc: string = 'assets/summary/done-checkmark.png';
 
+  onTodoHover(isHovering: boolean): void {
+    if (isHovering) {
+      this.todoIconSrc = 'assets/summary/edit-black-signup.png';
+    } else {
+      this.todoIconSrc = 'assets/summary/edit-white-signup.png';
+    }
+  }
+
+  onDoneHover(isHovering: boolean): void {
+    if (isHovering) {
+      this.doneIconSrc = 'assets/summary/done-checkmark-hover.png';
+    } else {
+      this.doneIconSrc = 'assets/summary/done-checkmark.png';
+    }
+  }
 }
